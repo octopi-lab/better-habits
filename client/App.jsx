92d4ff80a239353569cd304b90c158e277e861fe
JakeGillan <126023939+JakeGillan@users.noreply.github.com>
2023-04-07 17:27:25 -0500

    Original Boiler-Plate
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Cartlong7 <Cartlong7@users.noreply.github.com>
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Suigin1 <Suigin1@users.noreply.github.com>

---
+++ client/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component, element } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+
+
+class App extends Component {
+
+   render() {
+    return (
+        <div>
+           <h1>TEST TEST</h1>
+        </div>
+        );   
+    };
+};
+
+export default App; 
