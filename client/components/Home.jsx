98bb5af3fa90b57ed0202c4e726329ecf32bb3a4
Samuel Lee <leesamuel423@gmail.com>
2023-04-08 14:18:13 -0400

    insert full descriptions for homepage
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Cartlong7 <Cartlong7@users.noreply.github.com>
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Suigin1 <Suigin1@users.noreply.github.com> "

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,32 +4,83 @@ class Home extends Component {
   render() {
     return (
       <main>
-        <div className="image-container">
-          <img src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Image 1" />
-          <p className= "about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, doloremque consequatur, corporis nobis veritatis dolor, hic iure laudantium eveniet blanditiis omnis facere. Quo obcaecati fugit asperiores aliquam assumenda, magnam accusantium!
-          Nemo repellendus mollitia veritatis ducimus reiciendis deserunt! Tenetur, atque. Assumenda illum at architecto tempore soluta nihil fuga voluptas odit fugit impedit repudiandae, laborum expedita, ducimus, eveniet sunt fugiat magnam veniam?
-          Numquam, totam nobis pariatur quia iusto, non nulla doloribus aut tempora consequuntur ad veniam magnam deserunt eos quisquam. Amet ipsam corrupti illo aliquid beatae deserunt quae ratione. Dolores, eum nemo.
-          Amet similique neque tenetur! Architecto, quos. Optio, itaque earum iste fuga repellendus corporis voluptatum enim ullam reprehenderit illo cumque facere. Possimus temporibus tenetur similique perspiciatis itaque amet ratione, nihil sapiente.
-          Itaque, nesciunt nemo autem corporis veritatis illo quia impedit commodi laboriosam. Sapiente maxime consectetur enim accusantium voluptatem eligendi exercitationem, ad sed impedit voluptatum voluptates quia itaque autem, minus aspernatur explicabo.
-          A consequatur repudiandae eius aspernatur et cumque? Porro tempore, vel officiis fuga ratione adipisci atque quasi architecto obcaecati exercitationem voluptatum. Placeat eum expedita sit ipsa optio neque similique assumenda repudiandae!
-          Voluptates commodi enim similique ut nihil veniam quos! Similique, omnis delectus qui voluptatum explicabo provident cupiditate pariatur deleniti animi iusto id impedit? Voluptates blanditiis ipsum consequatur, iure dolores aperiam tempora.
-          Accusamus illo doloremque provident id impedit officia dolores nemo deserunt facere laborum ea nostrum, exercitationem doloribus excepturi amet molestiae alias odio sed. Sint quam dicta ratione excepturi facilis, incidunt ab!
-          Officia perspiciatis quasi culpa corporis reprehenderit ad impedit, accusantium unde autem ratione fuga numquam nobis consectetur? Incidunt sapiente deserunt illo quidem earum, odio ipsam autem, tempora quasi delectus, porro ullam!
-          Delectus perferendis illum error optio earum facere, rerum maxime quos exercitationem quam aut laudantium nisi a accusamus inventore tempora incidunt iure blanditiis expedita id ipsam quidem unde enim! Pariatur, iste.
-          Dicta quae ab pariatur perspiciatis consequuntur at, dolor iusto quibusdam neque incidunt quisquam vero voluptatibus. Animi corrupti vero, temporibus sequi accusamus ipsa magni recusandae. Optio minima molestias illum adipisci assumenda.
-          Consequatur, temporibus illum! Velit, culpa. Tempore quae porro iusto, reiciendis minima earum eaque corrupti velit quas commodi totam nostrum deleniti veniam inventore ea magni eveniet? Perspiciatis iusto eaque neque itaque.
-          Minus sequi possimus beatae explicabo omnis incidunt ab quo blanditiis delectus ex quae fuga qui optio impedit, ipsam maxime laborum commodi velit suscipit molestias saepe tempore, nisi dolore illum. Modi?
-          Repellat reprehenderit ipsum consectetur asperiores odio sapiente, eos magnam qui quae facere modi culpa aut nesciunt iste tempore, ut eveniet quas dolor voluptatibus! Sit vero alias ducimus ex minima tenetur!
-          A quibusdam iusto magnam tempore autem facere quis consequuntur non voluptatum, voluptate quaerat perspiciatis eveniet? Totam cumque, dolorem dignissimos cum possimus quidem repellendus rem enim vel, repudiandae excepturi inventore earum?
-          Vero, consequuntur nam debitis magnam non cum vel fuga atque tempore qui itaque nulla temporibus recusandae aspernatur repudiandae. Eaque cupiditate obcaecati tenetur mollitia quidem. Ut eveniet nostrum enim omnis amet.
-          Minus, culpa. Molestiae veniam dicta sapiente? Rem odit illo deleniti tenetur, quis at soluta quibusdam, et aperiam libero labore illum suscipit quas? Animi dolorum laudantium architecto sed ut, odio nostrum?
-          Ducimus atque consectetur beatae dignissimos recusandae vel architecto optio quasi at doloribus cumque quam amet adipisci, consequatur non! Cumque adipisci necessitatibus quis a aspernatur voluptatibus veniam? Vero ab sequi dolorem.
-          Error accusantium maxime excepturi nemo eius incidunt asperiores itaque, adipisci commodi architecto nihil deleniti! Labore, laboriosam culpa tenetur quam vero fugiat veniam dolorum et? Soluta quam recusandae perferendis omnis. Ea?
-          Temporibus non amet odit quo nulla soluta consectetur a in similique rem enim, itaque voluptas officiis ea perspiciatis cumque quisquam dolore, laborum harum ipsa quaerat architecto laboriosam reiciendis? Perspiciatis, laudantium.</p>
-        </div>
-        <div className= "side bar"> 
+          <img className="img" src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Image 1" />
+          <div className= "about">
+			<h1>Actually Build Your Habits!</h1>
+			<p>Having a hard time building habits and reaching your goals?</p>
+			<p> Better Habits helps you build and maintain healthy habits with a <em>new optimized system</em> for habit building!</p>
+			<div className="step">
+				<h2>Step 1: Choose a habit</h2>
+				<p>Choose a habit you want to achieve!</p>
+			</div>
+			<div className="step">
+				<h2>Step 2: Choose up to 3 alternative actions for the habit</h2>
+				<p>This allows for flexibility and opportunities for consistency</p>
+			</div>
+			<div className="step">
+				<h3>Step 3: Define points criteria for the habit and the alternative actions</h3>
+				<p>Point allocations:</p>
+				<ul>
+					<li>1 point: Show Up</li>
+					<li>2 points: Easy</li>
+					<li>3 points: Medium</li>
+					<li>4 points: Hard</li>
+				</ul>
+			</div>
+			<div className="step">
+				<h3>Step 4: Go achieve your goals!</h3>
+				<p>Seize the day and record your progress~</p>
+			</div>
 
-        </div>
+
+			<h1 className="example-habit-building">Example: Habit</h1>
+			<div className="step">
+				<h2>Step 1</h2>
+				<p>I want to go to the gym every day!</p>
+			</div>
+			<div className="step">
+				<h2>Step 2</h2>
+				<p>If I can't go to the gym, my alternate habits are:</p>
+				<ul>
+					<li>Go for a walk</li>
+					<li>Do 20 pushups and 20 situps</li>
+					<li>20 pullups</li>
+				</ul>
+			</div>
+			<div className="step">
+				<h2>Step 3</h2>
+				<p>Go to the gym</p>
+					<ul>
+						<li>1 point: Show Up</li>
+						<li>2 points: 30 minutes</li>
+						<li>3 points: 1 hour</li>
+						<li>4 points: 2 hours</li>
+					</ul>
+				<ul>
+					<li>Go for a walk</li>
+					<ul>
+						<li>1 point: Walk Around the Block for 15 minutes</li>
+						<li>2 points: Walk Around the Block for 30 minutes</li>
+						<li>3 points: Walk Around the Block for 1 hour</li>
+						<li>4 points: Walk Around the Block for 1.5 hours</li>
+					</ul>
+					<li>Do pushups and situps</li>
+						<ul>
+							<li>1 point: Do 20 Pushups and 20 Situps</li>
+							<li>2 points: Do 50 pushups and 50 situps</li>
+							<li>3 points: Do 80 pushups and 80 situps</li>
+							<li>4 points: Do 100 pushups and 100 situps</li>
+						</ul>
+					<li>Play with the dog, no sitting down!</li>
+						<ul>
+							<li>1 point: Play with the dog for 20 minutes</li>
+							<li>2 points: Play with the dog for 30 minutes</li>
+							<li>3 points: Play with the dog for 40 minutes</li>
+							<li>4 points: Play with the dog for 1 hour</li>
+						</ul>
+				</ul>
+		  	</div>
+		</div>
       </main>
     );
   }
