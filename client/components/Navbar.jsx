1130704d00e78962783aaf141e55887d20d062dc
Samuel Lee <leesamuel423@gmail.com>
2023-04-07 20:59:32 -0400

    setup initial outline of navbar
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Cartlong7 <Cartlong7@users.noreply.github.com>
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Suigin1 <Suigin1@users.noreply.github.com>

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { Components } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+	return(
+		<header>
+			<Link to="/" className="logo">RepCode</Link>
+			<nav>
+				<ul>
+					<li><Link to="/about">About</Link></li>
+					<li><Link to="/contact">Contact</Link></li>
+					<div className="reg-buttons">
+						<li><Link to="/login">Login</Link></li>
+						<li><Link to="/signup">Sign Up</Link></li>
+					</div>
+				</ul>
+			</nav>
+		</header>
+
+	)
+}
+
+export default Headers;
