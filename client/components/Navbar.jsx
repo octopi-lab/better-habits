2008ab62cd95185ba27be5da5216eac176c8ac05
Samuel Lee <leesamuel423@gmail.com>
2023-04-08 11:22:23 -0400

    add className to navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return(
-		<header>
+		<header className="navbar">
 			<Link to="/" className="logo">RepCode</Link>
 			<nav>
 				<ul>
