6cf45929d91358a2d8cf356a08b49c6ca37778b5
Suigin1 <justin.dilts@mail.utoronto.ca>
2023-04-08 18:45:29 -0400

    frontend and styling for login and signup pages initially complete Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com> Co-authored-by: Cartlong7 <Cartlong7@users.noreply.github.com> Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com> Co-authored-by: Jake Gillan <jakegillan@users.noreply.github.com>

---
+++ client/components/Login.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom'
+
+function Login() {
+  return (
+    <section className='login-box'>
+      <form method='POST' action='/login'>
+        <input name='username' type='text' placeholder='Username'></input>
+        <input name='password' type='password' placeholder='Password'></input>
+        <input type='submit' value='Log In'></input>
+      </form>
+    </section>
+  )
+}
+
+export default Login;
