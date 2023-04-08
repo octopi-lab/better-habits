160ffdd7c70b671b0875886bbc68d986b0df6bfb
JakeGillan <126023939+JakeGillan@users.noreply.github.com>
2023-04-07 19:51:50 -0500

    first push - home page

--- client/App.jsx
+++ client/App.jsx
@@ -2,14 +2,21 @@ import React, {Component, element } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 
-
+import Home from './components/Home.jsx'
 
 class App extends Component {
 
    render() {
     return (
         <div>
-           <h1>TEST TEST</h1>
+           <nav></nav>
+            <main>
+            <Routes> 
+                <Route  path = '/' element={<Home/>}></Route>
+                
+
+            </Routes>
+            </main>
         </div>
         );   
     };
