49c9f9931b4e1f7121e1bed566498b9a81d68c09
Samuel Lee <leesamuel423@gmail.com>
2023-04-10 11:31:53 -0400

    rough draft of homepage
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Cartlong7 <Cartlong7@users.noreply.github.com>
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Suigin1 <Suigin1@users.noreply.github.com>

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
 
 function Home() {
 
     return (
-      <h1>HomePage</h1>
+			<div className="home">
+				<div className="welcome">
+					<h1>Welcome to Better Habits</h1>
+					<h2>No more giving up on your habit goals!</h2>
+					<p>Achieve success and change your life with a new revolutionary habit buidling system!</p>
+					<h2>Watch this video!</h2>
+					<p>THERE WILL BE THAT YOUTUBE VIDEO HERE!!!</p>
+				</div>
+				<div className="logsign-forms">
+					<Login />
+					<Signup />
+				</div>
+			</div>
     );
 }
 
